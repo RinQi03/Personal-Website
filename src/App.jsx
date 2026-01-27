57c02b5556af0663ce769b1e7290fb140688a049
RinQi03 <lea.qi.22@gmail.com>
2026-01-27 00:52:23 -0500

    github actions for pages revise7

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { HashRouter as Router, Routes, useLocation } from "react-router-dom";
+import { HashRouter, Route, Routes, useLocation } from "react-router-dom";
 // import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from "./components/Navbar";
 import { Home, About, Projects, Contact, Test, Experience, Life } from "./pages";
